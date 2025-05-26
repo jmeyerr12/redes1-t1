5f6f0a74250f4ac0a4d67d6551b585116a4bf3ed
jmeyerr12 <joaomeyermuhlmann@gmail.com>
2025-05-26 15:26:44 -0300

    a

--- cliente.c
+++ cliente.c
@@ -73,7 +73,10 @@ void receber_arquivo(int tipo, const char *nome_arquivo, int tamanho) {
 
             if (tipo == TEXT_ACK_NAME) {
                 char comando[128];
-                snprintf(comando, sizeof(comando), "gedit \"%s\" &", nome_arquivo);
+                snprintf(comando, sizeof(comando), "nano \"%s\" &", nome_arquivo);
+                system(comando);
+            } else if (tipo == IMG_ACK_NAME || tipo == VIDEO_ACK_NAME) {
+                snprintf(comando, sizeof(comando), "xdg-open \"%s\" &", nome_arquivo);
                 system(comando);
             }
             return;
